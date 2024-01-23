76af468ca80b09442e765aff8aa09d3b9ee44eb6
Gloriaza Yemima Lawrance Odyardy <gloriazayemima@gmail.com>
2024-01-23 09:02:40 +0700

    create user recommedation page

--- src/pages/UserRecommendationPage.jsx
+++ src/pages/UserRecommendationPage.jsx
@@ -1,6 +1,71 @@
+import React from "react";
+
+import SidebarProfile from "../components/SidebarProfile";
+import CardClass from "../components/CardClass";
+import ButtonRecommendation from "../components/ButtonRecommendation";
+
+import { useNavigate } from "react-router-dom";
+
+import "../index.css";
+
 function UserRecommendationPage() {
+  const navigate = useNavigate();
+  // dummy data
+  const classData = {
+    classes: [
+      {
+        id: 1,
+        image: "https://dummyimage.com/400x400/000/fff.jpg&text=Class+Image",
+        username: "janedoe3",
+        name: "Introduction to Graphic Design 1",
+        paid: true,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, culpa.",
+        link: "https://course.com/course",
+        rating: 4.5,
+      },
+      {
+        id: 2,
+        image: "https://dummyimage.com/400x400/000/fff.jpg&text=Class+Image",
+        username: "janedoe3",
+        name: "Introduction to Graphic Design 2",
+        paid: false,
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque, culpa.",
+        link: "https://course.com/course",
+        rating: 4.3,
+      },
+    ],
+  };
+
   return (
-    <div></div>
+    <>
+      <div
+        className="flex flex-row gap-12"
+        style={{ fontFamily: "var(--paragraph-font)" }}
+      >
+        {/* sidebar */}
+        <div>
+          <SidebarProfile />
+        </div>
+        {/* user's class recommendation (card) */}
+        <div className="green mt-6 flex flex-col align-middle justify-center">
+              {classData.classes.map((i) => (
+                <div key={i.id} className="mb-2">
+                  <CardClass data={i} editBtn={false}></CardClass>
+                </div>
+              ))}
+            {/* button recommendation */}
+            <div>
+              <ButtonRecommendation
+                name="Kelas"
+                padding="px-4"
+                action={() => navigate("/recommendations")}
+              />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
