95ce5157fb3d9dbb026305654626687db0368242
syahba <ksyaahba@gmail.com>
2024-01-31 15:10:22 +0700

    create get, post, put, patch redux class

--- src/redux/slices/courseSlice.js
+++ src/redux/slices/courseSlice.js
@@ -5,14 +5,20 @@ const courseSlice = createSlice({
   name: 'course',
   initialState: {
     course: [],
-    courseDetail: {}
+    courseDetail: {},
+    message: ''
   },
   reducers: {
     setCourse(state, action) {
-      state.course = action.payload;
+      state.course = action.payload.data;
+      state.message = action.payload.message;
     },
     setCourseDetail(state, action) {
-      state.courseDetail = action.payload;
+      state.courseDetail = action.payload.data;
+      state.message = action.payload.message;
+    },
+    setMessage(state, action) {
+      state.message = action.payload;
     }
   }
 });
@@ -22,7 +28,7 @@ export const { setCourse, setCourseDetail } = courseSlice.actions;
 // function get class by roadmap
 export const getClassRoadmap = (jobId, roadmapId) => {
   return async (dispatch) => {
-    const { data: { data } } = await axios({
+    const { data } = await axios({
       method: 'get',
       url: `https://skilled-women-be-production.up.railway.app/jobs/${jobId}/roadmaps/${roadmapId}/classes`,
       responseType: 'json'
@@ -33,13 +39,82 @@ export const getClassRoadmap = (jobId, roadmapId) => {
 };
 
 // function get detail class
+export const getClass = (token, classId) => {
+  return async (dispatch) => {
+    const { data } = await axios({
+      method: 'get',
+      url: `https://skilled-women-be-production.up.railway.app/classes/${classId}`,
+      headers: {
+        'Authorization': token
+      },
+      responseType: 'json'
+    });
+
+    dispatch(setCourseDetail(data));
+  };
+};
 
 // function submit class recommendation
+export const submitClass = (token, payload) => {
+  return async (dispatch) => {
+    const { data: { message } } = await axios({
+      method: 'post',
+      url: 'https://skilled-women-be-production.up.railway.app/classes',
+      headers: {
+        'Authorization': token
+      },
+      data: payload
+    });
+
+    dispatch(setMessage(message));
+  };
+};
 
 // function edit class recommendation
+export const editClass = (token, classId, payload) => {
+  return async (dispatch) => {
+    const { data: { message } } = await axios({
+      method: 'put',
+      url: `https://skilled-women-be-production.up.railway.app/classes/${classId}`,
+      headers: {
+        'Authorization': token
+      },
+      data: payload
+    });
+
+    dispatch(setMessage(message));
+  };
+};
 
 // function vote class
+export const voteClass = (token, classId, payload) => {
+  return async (dispatch) => {
+    const { data: { message } } = await axios({
+      method: 'patch',
+      url: `https://skilled-women-be-production.up.railway.app/classes/${classId}`,
+      headers: {
+        'Authorization': token
+      },
+      data: payload
+    });
+
+    dispatch(setMessage(message));
+  };
+};
 
 // function delete class
+export const deleteClass = (token, classId) => {
+  return async (dispatch) => {
+    const { data: { message } } = await axios({
+      method: 'post',
+      url: `https://skilled-women-be-production.up.railway.app/classes/${classId}`,
+      headers: {
+        'Authorization': token
+      }
+    });
+
+    dispatch(setMessage(message));
+  };
+};
 
 export default courseSlice.reducer;
