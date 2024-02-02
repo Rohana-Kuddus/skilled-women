aa443bdecfe1979fcf8cba1e95ed6ceb45009124
syahba <ksyaahba@gmail.com>
2024-02-02 16:25:09 +0700

    create register slice

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,22 +1,39 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { setStatusCode } from "./courseSlice";
 
 const authSlice = createSlice({
   name: 'auth',
   initialState: {
-    session: false // status logged in atau tidak (false = tidak login, true = sudah login)
+    session: false, // status logged in atau tidak (false = tidak login, true = sudah login)
+    statusCode: 200
   },
   reducers: {
     setSession(state, action) {
       state.session = action.payload;
+    },
+    setStatusCode(state, action) {
+      state.statusCode = action.payload;
     }
   }
 });
 
-export const { setSession } = authSlice.actions;
+export const { setSession, setStatusCode } = authSlice.actions;
 
 // function cek user login
 
 // function register
+export const registerUser = (payload) => {
+  return async (dispatch) => {
+    const { status } = await axios({
+      method: 'post',
+      url: 'https://skilled-women-be-production.up.railway.app/auth/register',
+      data: payload
+    });
+
+    dispatch(setStatusCode(status));
+  };
+};
+
 
 // function login + input session cookies
 
